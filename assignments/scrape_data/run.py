5c8fa5fb0eebb9f62cc6578fd2ec3090dfa38e8f
Shenglin Sun <sunshenglin12@gmail.com>
2017-10-20 23:18:51 -0500

    Add files via upload

---
+++ assignments/scrape_data/run.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+import time
+import random
+import json
+
+Movie_names = []
+
+for i in range(0,6):
+    i=str(i+1)
+    url = "http://www.yifysubtitles.com/browse/page-"+i
+    print(url)
+    html = requests.get(url).text.split('\n')
+
+    for line in html:
+        found = line.find('class="media-object"')
+
+        if found > 0:
+            line = line[found:]
+            pos = line.find('alt="')
+
+            Movie_names.append(line[pos + 5:-34])
+
+
+    time.sleep(random.randint(0, 1))
+print (Movie_names)
+
+with open('try.json','w') as file:
+    movie=json.dumps(Movie_names,indent=1)
+    file.write(movie)
